9cb276548503b22f6e9214ccb046afbf95d85820
Logan Ryan <loganryan982@gmail.com>
2021-02-06 12:02:40 +1100

    Setup aieBootstrap

---
+++ Project2D/PhysicsApp.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Application.h"
+#include "Renderer2D.h"
+
+class PhysicsApp : public aie::Application {
+public:
+
+	PhysicsApp();
+	virtual ~PhysicsApp();
+
+	virtual bool startup();
+	virtual void shutdown();
+
+	virtual void update(float deltaTime);
+	virtual void draw();
+
+protected:
+
+	aie::Renderer2D*	m_2dRenderer;
+	aie::Texture*		m_texture;
+	aie::Texture*		m_shipTexture;
+	aie::Font*			m_font;
+
+	float m_timer;
+};
